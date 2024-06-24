60e279be283a1e8eb721c050c4cbe3eeffeee030
athenacats <chenalonya@gmail.com>
2024-06-24 10:12:29 +0300

    Include search bar html and css

--- footer.php
+++ footer.php
@@ -1,3 +1,12 @@
+<footer class="footer text-center py-4">
+
+
+    <p class="copyright"><a href="https://chena.dev/">Copyright &copy; Freaky link 2024</a></p>
+
+
+</footer>
+
+
 
 <?php wp_footer(); ?>
 
