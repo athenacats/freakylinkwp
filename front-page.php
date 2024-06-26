1fa9657baac4861199e303bdc157320385f2049c
athenacats <chenalonya@gmail.com>
2024-06-26 13:06:36 +0300

    Test create loop to pull in data from WP

--- front-page.php
+++ front-page.php
@@ -1,6 +1,15 @@
 <?php get_header(); ?>
 
 <div class="main-wrapper">
+    <?php 
+    if(have_posts(  )){
+        while(have_posts(  )) {
+            the_post(  );
+            the_content(  );
+        }
+    }
+    
+    ?>
     <div class="introduction">
 
         <div class="content">
