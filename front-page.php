2381ea46da2cc2432100d5d5d7bc49aa73a2b376
athenacats <chenalonya@gmail.com>
2024-10-07 12:50:29 +0300

    Change layout css somewhat

--- front-page.php
+++ front-page.php
@@ -1,14 +1,14 @@
 <?php get_header(); ?>
 
-<div class="main-wrapper">
-    <?php 
-    if(have_posts(  )){
-        while(have_posts(  )) {
-            the_post(  );
-            the_content(  );
+<div class="main-wrapper px-3 py-5 p-md-5">
+    <?php
+    if (have_posts()) {
+        while (have_posts()) {
+            the_post();
+            the_content();
         }
     }
-    
+
     ?>
     <div class="introduction">
 
