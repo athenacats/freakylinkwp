9ab387ec5714fb948d582b905340566ecc2ef1dd
athenacats <chenalonya@gmail.com>
2024-10-16 09:52:06 +0300

    Set up archive page

---
+++ archive.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+
+
+<div class="main-wrapper px-3 py-5 p-md-5">
+    <h1 class="heading text-center"><?php the_title() ?></h1>
+    <?php
+    if (have_posts()) {
+        while (have_posts()) {
+            the_post();
+
+            get_template_part('template-parts/content', 'archive');
+        }
+    }
+
+    ?>
+
+</div>
+</div>
+
+
+<?php get_footer(); ?>
