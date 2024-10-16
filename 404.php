accecd27e1bfd0fe2c5d7f61c7b731d34b5e7cea
athenacats <chenalonya@gmail.com>
2024-10-16 10:43:00 +0300

    Create 404 template

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+
+<div class="main-wrapper px-3 py-5 p-md-5">
+    <h1 class="heading text-center">Page Not Found</h1>
+    <div class="d-flex justify-content-center pt-4"><?php
+                                                    get_search_form()
+                                                    ?></div>
+
+
+
+</div>
+</div>
+
+
+<?php get_footer(); ?>
